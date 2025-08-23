07188192d7a40acad1c905a4fe46590ebc6394da
malekith089 <malekith089@gmail.com>
2025-08-23 14:02:01 +0700

    typo fix

--- components/sponsors-section.tsx
+++ components/sponsors-section.tsx
@@ -52,7 +52,7 @@ export function SponsorsSection() {
     },
     {
       name: "Ritma FMIPA UB",
-      logo: "/images/partners/ritma.webp",
+      logo: "/images/partners/ritma.png",
       website: "#",
     },
   ]
